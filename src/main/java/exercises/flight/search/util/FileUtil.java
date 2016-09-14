1f42fadbe318556e6d1b313d10547f109e0544e7
Toni Mecha <toni.mecha@gmail.com>
2016-09-14 20:02:56 +0200

    No need to catch only to rethrow

--- src/main/java/exercises/flight/search/util/FileUtil.java
+++ src/main/java/exercises/flight/search/util/FileUtil.java
@@ -7,11 +7,8 @@ import java.nio.file.*;
 public class FileUtil {
 
     public static Reader getFileReader(String pathName, String fileName) throws IOException {
-        try {
-            Path path = Paths.get("src/test/resources", "sample.csv");
-            return Files.newBufferedReader(path, Charset.forName("UTF-8"));
-        } catch (IOException ex) {
-            throw ex;
-        }
+
+        Path path = Paths.get("src/test/resources", "sample.csv");
+        return Files.newBufferedReader(path, Charset.forName("UTF-8"));
     }
 }
